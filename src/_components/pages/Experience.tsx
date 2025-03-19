98e04f5b70e7ff9095e0f50ae7cb309f76834f8a
Pilner <raileyvictuelles@gmail.com>
2025-03-19 14:43:56 +0800

    feat: add chevron animation

--- src/_components/pages/Experience.tsx
+++ src/_components/pages/Experience.tsx
@@ -31,9 +31,24 @@ export default function Experience() {
 						I make things <span className="text-sub-accent">work</span>
 					</h1>
 					<div className="absolute bottom-10 flex flex-col">
-						<FontAwesomeIcon size="2xl" color="black" icon={faChevronDown} />
-						<FontAwesomeIcon size="2xl" color="black" icon={faChevronDown} />
-						<FontAwesomeIcon size="2xl" color="black" icon={faChevronDown} />
+						<FontAwesomeIcon
+							size="2xl"
+							color="black"
+							icon={faChevronDown}
+							className="animation-delay-0 animate-pulse"
+						/>
+						<FontAwesomeIcon
+							size="2xl"
+							color="black"
+							icon={faChevronDown}
+							className="animation-delay-1 animate-pulse"
+						/>
+						<FontAwesomeIcon
+							size="2xl"
+							color="black"
+							icon={faChevronDown}
+							className="animation-delay-2 animate-pulse"
+						/>
 					</div>
 				</div>
 			</section>
