4a53f5eb6ae6a9a5e4e5025b4487045591f3a236
Railey Macbook Pro <raileyvictuelles@gmail.com>
2025-04-03 12:24:01 +0800

    feat: about mobile responsive

--- src/_components/pages/About.tsx
+++ src/_components/pages/About.tsx
@@ -54,7 +54,7 @@ export default function About() {
 		<>
 			<section className="h-screen w-full">
 				<div className="relative container flex h-full w-full items-center justify-center">
-					<h1 className="font-main text-7xl font-semibold">
+					<h1 className="font-main xs:text-6xl text-5xl font-semibold lg:text-7xl">
 						This is <span className="text-sub-accent">me</span>
 					</h1>
 					<div className="absolute bottom-10 flex flex-col">
@@ -79,25 +79,25 @@ export default function About() {
 					</div>
 				</div>
 			</section>
-			<section className="h-screen w-full">
-				<div className="container flex h-full gap-4">
+			<section className="flex min-h-screen w-full items-center py-8 lg:min-h-full">
+				<div className="container flex h-full flex-col-reverse gap-16 lg:flex-row lg:gap-4">
 					<div className="flex-1 basis-1/2 items-center justify-center gap-4 self-center">
 						<JsonBlock data={infoJson}></JsonBlock>
 					</div>
 					<div className="flex basis-1/2 items-center justify-center">
-						<div className="overflow relative aspect-square h-auto w-3/4">
+						<div className="overflow relative aspect-square h-auto w-3/4 sm:w-3/5 lg:w-3/4">
 							<Image
 								src="/images/portraits/my_picture.jpg"
 								alt="My Picture"
 								fill
-								className="relative z-10 rounded-xl object-contain shadow-lg"
+								className="relative z-10 rounded-xl object-contain drop-shadow-lg"
 							/>
 							<Image
 								src="/images/background/intro-pic-bg.svg"
 								alt="My Picture Background"
 								width={0}
 								height={0}
-								className="absolute top-5 left-5 z-0 h-full w-full rounded-xl object-cover shadow-lg"
+								className="absolute top-3 left-3 z-0 h-full w-full rounded-xl object-cover shadow-lg sm:top-4 sm:left-4 xl:top-5 xl:left-5"
 							/>
 						</div>
 					</div>
