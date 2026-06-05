405aa294785e107e07a12aceba44b1c9226fe921
Railey Victuelles M4 Air <raileyvictuelles@gmail.com>
2026-06-06 00:17:49 +0800

    feat: enhance navbar

--- src/components/layouts/HomeNavbar.tsx
+++ src/components/layouts/HomeNavbar.tsx
@@ -1,3 +1,5 @@
+import { Link, useLocation } from 'react-router';
+
 import { faAnglesLeft, faBriefcase, faDiagramProject, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -29,6 +31,8 @@ const tabs = [
 ];
 
 export default function HomeNavbar() {
+  const { pathname } = useLocation();
+
   return (
     <div className='group fixed right-0 flex h-full items-center justify-center'>
       <div className='visible absolute duration-250 ease-in-out group-hover:invisible group-hover:opacity-0'>
@@ -40,16 +44,18 @@ export default function HomeNavbar() {
           {tabs.map((tab) => (
             <li
               key={tab.name}
-              className={
-                'group/item border-primary-black/50 hover:bg-primary-black bg-primary-white hover:text-primary-white relative aspect-square h-12 cursor-pointer rounded-full border transition-[color,margin] duration-250'
-              }
+              className={`group/item border-primary-black/50 relative aspect-square h-12 rounded-full border transition-[color,margin] duration-250 ${pathname === tab.url ? 'bg-primary-black text-primary-white' : 'hover:bg-primary-black hover:text-primary-white bg-primary-white'}`}
             >
-              <a href={tab.url} className='flex h-full w-full items-center justify-center'>
+              <Link
+                to={tab.url}
+                aria-disabled={pathname === tab.url ? 'true' : 'false'}
+                className={`flex h-full w-full items-center justify-center ${pathname === tab.url ? 'cursor-default' : 'cursor-pointer'}`}
+              >
                 <FontAwesomeIcon icon={tab.icon} />
                 <div className='text-primary-black absolute top-1/2 right-0 z-199 flex translate-x-0 -translate-y-1/2 items-center justify-center rounded-lg px-1 opacity-0 transition-all duration-500 group-hover/item:top-1/2 group-hover/item:right-[125%] group-hover/item:opacity-100'>
                   <p className='text-primary-black small font-bold'>{tab.text}</p>
                 </div>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
