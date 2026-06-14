c287766c4808d9474d94094448fdaca3a1814342
Railey Victuelles M4 Air <raileyvictuelles@gmail.com>
2026-06-15 01:26:50 +0800

    feat: add page transitions using motion

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,11 +1,26 @@
+import { AnimatePresence, motion } from 'motion/react';
+
 import HomeNavbar from '@src/components/layouts/HomeNavbar';
-import { Outlet } from 'react-router';
+import { useLocation, useOutlet } from 'react-router';
 
 export default function HomeLayout() {
+  const location = useLocation();
+  const outlet = useOutlet();
+
   return (
     <div className='h-dvh'>
       <HomeNavbar />
-      <Outlet />
+      <AnimatePresence mode='wait'>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3, ease: 'easeInOut' }}
+        >
+          {outlet}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
